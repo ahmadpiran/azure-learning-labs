682172dc6b4e95a7ba51fcde48d8bbce75d39ca6
ahmadpiran <ahmad.piran@gmail.com>
2025-10-11 15:30:16 +0300

    first commit of azure-bastion lab

---
+++ azure-bastion/variables.tf
@@ -0,0 +1,29 @@
+variable "subscription_id" {
+  type        = string
+  description = "Enter subscription id in cmd"
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "resource_group_location" {
+  type        = string
+  default     = "francecentral"
+  description = "Location of the resource group"
+}
+
+variable "username" {
+  type        = string
+  default     = "azureuser"
+  description = "Admin username for the VM"
+}
+
+variable "password" {
+  type        = string
+  default     = "A1s2d3f4g5!"
+  description = "Admin password for the VM"
+  sensitive   = true
+}
